9d97c96b51bb13941b0040a52eee354efc00210a
Sojib Ali <sojib.ali@hotmail.com>
2024-12-19 23:30:13 +0600

    added databases id for appwrite

--- social/src/_auth/forms/SigninForm.tsx
+++ social/src/_auth/forms/SigninForm.tsx
@@ -5,4 +5,6 @@ const SigninForm = () => {
   )
 }
 
+
+
 export default SigninForm
--- social/src/lib/appwrite/api.ts
+++ social/src/lib/appwrite/api.ts
@@ -11,6 +11,8 @@ export async function createUserAccount(user: INewUser) {
             user.name
         )
         return newAccount;
+
+
     } catch (error) {
         console.log(error);
         return error;
